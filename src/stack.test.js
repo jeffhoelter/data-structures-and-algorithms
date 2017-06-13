d186cd633a3f7ed297f695946740b2a6e746ac69
jeffhoelter <jeffh@loar.net>
2017-06-13 09:28:46 -0400

    Implemented basic Stack.

---
+++ src/stack.test.js
@@ -0,0 +1,39 @@
+const Stack = require("./Stack");
+
+test("peek simple test", () => {
+  var myStack = new Stack();
+  myStack.push("Test1");
+  expect(myStack.peek()).toBe("Test1");
+  myStack.push("Test2");
+  expect(myStack.peek()).toBe("Test2");
+  myStack.push("Test3");
+  expect(myStack.peek()).toBe("Test3");
+  myStack.push("Test4");
+  expect(myStack.peek()).toBe("Test4");
+});
+
+test("pop test", () => {
+  var myStack = new Stack();
+  myStack.push("Test1");
+  myStack.push("Test2");
+  myStack.push("Test3");
+  myStack.push("Test4");
+  expect(myStack.pop()).toBe("Test4");
+  expect(myStack.count()).toBe(3);
+});
+
+test("pop test complicated", () => {
+  var myStack = new Stack();
+  myStack.push("Test1");
+  myStack.push("Test2");
+  myStack.push("Test3");
+  myStack.push("Test4");
+  expect(myStack.pop()).toBe("Test4");
+  expect(myStack.count()).toBe(3);
+  expect(myStack.pop()).toBe("Test3");
+  expect(myStack.count()).toBe(2);
+  expect(myStack.pop()).toBe("Test2");
+  expect(myStack.count()).toBe(1);
+  expect(myStack.pop()).toBe("Test1");
+  expect(myStack.count()).toBe(0);
+});
